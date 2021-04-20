8cd7ba2fba903fb94670451da18cdf680784ca89
sismadi co <36598164+sismadico@users.noreply.github.com>
2021-04-20 23:10:42 +0700

    Delete pwa directory

--- pwa/register-worker.js
+++
@@ -1,8 +0,0 @@
-/**
- * Register the service worker
- */
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('../sw.js')
-		.then(function () { console.log('Service Worker Registered'); });
-}
